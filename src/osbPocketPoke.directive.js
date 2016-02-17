d1e99a0a405af25f72ff7e7058eb8bf3bb53d2cc
Ben Burton <sistematic3@yahoo.co.uk>
2016-02-17 15:40:15 +0000

    Adding missing functions and tidy up

--- src/osbPocketPoke.directive.js
+++ src/osbPocketPoke.directive.js
@@ -30,11 +30,53 @@ angular.module('sistem3.osb-pocket-poke', ['osb-pocket-poke-template'])
                     $scope.pokemonSprites = JSON.parse(pokeSpriteCache);
                 }
 
+                $scope.setPokeSprite = function(data) {
+                    var sprite = new Image();
+                    var spriteHolder = document.querySelector('#sprite-' + data.pokemon.name + '.img-holder');
+                    spriteHolder.appendChild(sprite);
+                    sprite.src = $scope.pokemonApiBase + data.image;
+                };
+
+                $scope.getPokeSprite = function(pokemon, cached) {
+                    console.log(pokemon);
+                    if (cached && $scope.pokemonSprites.length > 1) {
+                        $scope.pokemonSprites.forEach(function(element, index, array) {
+                            if (pokemon.name === element.pokemon.name) {
+                                setTimeout(function() {
+                                    $scope.setPokeSprite(element);
+                                }, 250);
+                            }
+                        });
+                        return false;
+                    }
+                    // Fix for glitch in sprite ordering
+                    var updatedId = parseInt(pokemon.id) + 1;
+                    console.log(updatedId);
+                    fetch($scope.pokemonApiBase + '/api/v1/sprite/' + updatedId)
+                        .then(function(response) {
+                            if (response.status !== 200) {
+                                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                                return;
+                            }
+                            response.json().then(function(data) {
+                                console.log(data);
+                                $scope.pokemonSprites.push(data);
+                                localStorage.setItem('osbPocketPoke.sprites', JSON.stringify($scope.pokemonSprites));
+                                $scope.setPokeSprite(data);
+                            });
+                        })
+                        .catch(function(err) {
+                            console.log('Failed');
+                        });
+                };
+
                 $scope.getPokemon = function(pokemon, newList) {
-                    //console.log(pokemon);
+                    console.log(pokemon);
+                    console.log(newList);
                     if (!newList) {
                         if ($scope.pokemon.length > 1) {
                             $scope.pokemon.forEach(function(element, index, array) {
+                                console.log(element);
                                 if (pokemon.name === element.name) {
                                     $scope.getPokeSprite(element, true);
                                 }
@@ -42,7 +84,7 @@ angular.module('sistem3.osb-pocket-poke', ['osb-pocket-poke-template'])
                             return false;
                         }
                     }
-                    /*fetch(pokemon.url)
+                    fetch(pokemon.url)
                         .then(function(response) {
                             if (response.status !== 200) {
                                 console.log('Looks like there was a problem. Status Code: ' + response.status);
@@ -52,17 +94,52 @@ angular.module('sistem3.osb-pocket-poke', ['osb-pocket-poke-template'])
                                 $scope.pokemon.push(data);
                                 if (!newList) {
                                     // Cache size too small for full listing storage
-                                    localStorage.setItem('osbPocketPoke.pokemon', JSON.stringify($scope..pokemon));
+                                    localStorage.setItem('osbPocketPoke.pokemon', JSON.stringify($scope.pokemon));
                                     $scope.getPokeSprite(data, true);
                                 } else {
-                                    console.log('set sprite');
+                                    console.log('Get sprite');
                                     $scope.getPokeSprite(data, false);
                                 }
                             });
                         })
                         .catch(function(err) {
                             console.log('Failed');
-                        });*/
+                        });
+                };
+
+                $scope.getMorePokemon = function() {
+                    var newListCount, updatedList;
+                    if ($scope.pokemon) {
+                        newListCount = $scope.listCount + $scope.pageLength;
+                        updatedList = $scope.pokemonList.slice($scope.listCount + 1, newListCount);
+                    } else {
+                        newListCount = $scope.pokemon.length + $scope.pageLength;
+                        updatedList = $scope.pokemonList.slice($scope.pokemon.length + 1, newListCount);
+                    }
+                    var callbackCount = 1;
+                    updatedList.forEach(function(element) {
+                        callbackCount++;
+                        console.log(callbackCount);
+                        if (callbackCount === $scope.pageLength) {
+                            console.log('Callback');
+                        }
+                        $scope.getPokemon(element, true);
+                    });
+                };
+
+                $scope.startScrollMagic = function() {
+                    var controller = new ScrollMagic.Controller();
+                    var scene = new ScrollMagic.Scene({triggerElement: '#loading-trigger', triggerHook: 'onEnter'})
+                        .addTo(controller)
+                        .on('enter', function (e) {
+                            //console.log(e);
+                            if (document.querySelector('#loading-trigger').className.indexOf('active') == -1) {
+                                document.querySelector('#loading-trigger').classList.add('active');
+                                console.log('Load more data');
+                                $scope.getMorePokemon();
+                            }
+                        });
+                    scene.update();
                 };
 
                 $scope.getPokeList = function() {
@@ -76,7 +153,7 @@ angular.module('sistem3.osb-pocket-poke', ['osb-pocket-poke-template'])
                         }, 1500);
                         return false;
                     }
-                    /*fetch($scope.pokemonApiBase + '/api/v2/pokemon/')
+                    fetch($scope.pokemonApiBase + '/api/v2/pokemon/')
                         .then(function(response) {
                             if (response.status !== 200) {
                                 console.log('Looks like there was a problem. Status Code: ' + response.status);
@@ -103,7 +180,7 @@ angular.module('sistem3.osb-pocket-poke', ['osb-pocket-poke-template'])
                         })
                         .catch(function(err) {
                             console.log('Failed');
-                        });*/
+                        });
                 };
             }
         };
